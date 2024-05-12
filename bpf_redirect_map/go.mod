783ea450bfee64cbcdefee27858f7827caf7be4e
Raihan-009 <islamraihan09@gmail.com>
2024-05-12 10:55:29 +0600

    bpf_map_redirect verified

--- bpf_redirect_map/go.mod
+++ bpf_redirect_map/go.mod
@@ -1,8 +1,13 @@
 module github.com/terassyi/go-xdp-examples/bpf_redirect_map
 
-go 1.16
+go 1.22
 
 require (
 	github.com/cilium/ebpf v0.7.0
 	github.com/vishvananda/netlink v1.1.0
+)
+
+require (
+	github.com/vishvananda/netns v0.0.0-20191106174202-0a2b9b5464df // indirect
+	golang.org/x/sys v0.0.0-20210906170528-6f6e22806c34 // indirect
 )
--- bpf_redirect_map/netns.sh
+++ bpf_redirect_map/netns.sh
@@ -3,30 +3,34 @@
 BUILD="build"
 CLEAN="clean"
 
+NODE1="node1"
+NODE2="node2"
+
+
 if [ "$BUILD" = "$1" ]; then
-	ip netns add node1
-	ip netns add node2
+	ip netns add "$NODE1"
+	ip netns add "$NODE2"
 	ip link add veth0 type veth peer veth1
 	ip link add veth2 type veth peer veth3
-	ip link set veth1 netns node1
-	ip link set veth2 netns node1
-	ip link set veth3 netns node2
+	ip link set veth1 netns "$NODE1"
+	ip link set veth2 netns "$NODE1"
+	ip link set veth3 netns "$NODE2"
 	ip addr add 192.168.0.3/24 dev veth0
-	ip netns exec node1 ip addr add 192.168.0.2/24 dev veth1
-	ip netns exec node1 ip addr add 192.168.1.4/24 dev veth2
-	ip netns exec node2 ip addr add 192.168.1.5/24 dev veth3
+	ip netns exec "$NODE1" ip addr add 192.168.0.2/24 dev veth1
+	ip netns exec "$NODE1" ip addr add 192.168.1.4/24 dev veth2
+	ip netns exec "$NODE2" ip addr add 192.168.1.5/24 dev veth3
 	ip link set up dev veth0
-	ip netns exec node1 ip link set up dev veth1
-	ip netns exec node1 ip link set up dev veth2
-	ip netns exec node1 ip link set up dev lo
-	ip netns exec node2 ip link set up dev veth3
-	ip netns exec node2 ip link set up dev lo
-	ip netns exec node1 ip route add 192.168.1.0/24 via 192.168.1.4 dev veth2
+	ip netns exec "$NODE1" ip link set up dev veth1
+	ip netns exec "$NODE1" ip link set up dev veth2
+	ip netns exec "$NODE1" ip link set up dev lo
+	ip netns exec "$NODE2" ip link set up dev veth3
+	ip netns exec "$NODE2" ip link set up dev lo
+	ip netns exec "$NODE1" ip route add 192.168.1.0/24 via 192.168.1.4 dev veth2
 	ip route add 192.168.1.0/24 via 192.168.0.2
 
 elif [ "$CLEAN" = "$1" ]; then
-	ip netns del node1
-	ip netns del node2
+	ip netns del "$NODE1"
+	ip netns del "$NODE2"
 else
 	echo "help:"
 	echo "	build: build a network to test with netns"
